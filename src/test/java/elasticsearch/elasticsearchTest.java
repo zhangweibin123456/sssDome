c0467c3ee7a5e7469bb042e11b6ca0765c61cd50
zhangweibin <zhangweibin@DESKTOP-KJI47G7>
2017-10-24 17:11:12 +0800

    学习

---
+++ src/test/java/elasticsearch/elasticsearchTest.java
@@ -0,0 +1,36 @@
+package elasticsearch;
+
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:elasticsearch/elasticsearch.xml" })
+public class elasticsearchTest {
+
+//	@Autowired
+//	private SimpleGateway simpleGateway;
+//
+//
+//	@Autowired
+//	private AbstractServerConnectionFactory crLfServer;
+	
+	
+//	@Before
+//	public void setup() {
+//		TestingUtilities.waitListening(this.crLfServer, 10000L);
+//	}
+//	
+	@Test
+	public void Test() {
+//		System.out.println("*********************start**************************");
+//		String result =simpleGateway.send("hashaishai");
+//		System.out.println(result);
+//		System.out.println("*********************end**************************");
+	}
+
+	
+}
